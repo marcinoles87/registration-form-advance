a8597577babb99195f119fe2e0b4762e4fdd5ea2
marcinoles87 <marcinoles87@gmail.com>
2023-02-23 20:48:08 +0100

    work with checkbox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
+import React,{useState} from 'react';
 import './App.css';
 import Form from './components/Form'
 
 function App() {
+
+
+
+  const [on,setOn] = useState(false);
+
+  const handleOn = () =>{
+    
+}
+
   return (
     <div className="App">
 
-      <div className='slider'><input className='slide' type={'button'}></input></div>
+      <div className='slider'><input className='slide' type={'button'} onClick={handleOn}></input></div>
       
       <Form></Form>
       
